4def35a6a5e92ef5a4bfb8d151e744ecdecfc293
emonmahdi <developeremon1997@gmail.com>
2023-07-20 12:35:52 +0600

    all books, reviews api [DONE]

--- index.js
+++ index.js
@@ -33,25 +33,117 @@ async function run() {
 
     // API
     app.get("/books", async (req, res) => {
+      const cursor = bookCollection.find({}).sort({ _id: -1 }).limit(10);
+      const books = await cursor.toArray();
+      res.send({ data: books });
+    });
+    app.get("/all-books", async (req, res) => {
       const cursor = bookCollection.find({});
       const books = await cursor.toArray();
       res.send({ data: books });
     });
 
-    // single book api
-    /*  app.get("/book/:id", async (req, res) => {
-      const id = req.params.id;
-      const book = { _id: ObjectId(id) };
-      const result = await bookCollection.findOne(book);
-      console.log(result);
-      res.send(result);
-    }); */
     app.get("/book/:id", async (req, res) => {
       const id = req.params.id;
 
       const result = await bookCollection.findOne({ _id: new ObjectId(id) });
       console.log(result);
       res.send(result);
+    });
+    app.get("/all-book/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await bookCollection.findOne({ _id: new ObjectId(id) });
+      console.log(result);
+      res.send(result);
+    });
+
+    /* app.put("/edit-book/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const body = req.body;
+
+      const result = await bookCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { body } }
+      );
+      res.send(result);
+    }); */
+    app.put("/edit-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const { title, author, genre, img, publication_date } = req.body; // Assuming your book has fields: title, author, and genre.
+
+      try {
+        const result = await bookCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { title, author, genre, img, publication_date } } // Update the specific fields you want to change
+        );
+
+        if (result.modifiedCount === 0) {
+          return res
+            .status(404)
+            .json({ error: "Book not found or no changes made." });
+        }
+
+        res.json({ message: "Book updated successfully" });
+      } catch (err) {
+        console.error("Error while updating book:", err);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.post("/book", async (req, res) => {
+      const book = req.body;
+      console.log(book);
+      const result = await bookCollection.insertOne(book);
+      res.send(result);
+    });
+
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await bookCollection.deleteOne({ _id: new ObjectId(id) });
+      console.log(result);
+      res.send(result);
+    });
+
+    app.post("/review/:id", async (req, res) => {
+      const productId = req.params.id;
+      const review = req.body.review;
+
+      console.log(productId);
+      console.log(review);
+
+      const result = await bookCollection.updateOne(
+        { _id: new ObjectId(productId) },
+        { $push: { reviews: review } }
+      );
+
+      console.log(result);
+
+      if (result.modifiedCount !== 1) {
+        console.error("Product not found or comment not added");
+        res.json({ error: "Product not found or comment not added" });
+        return;
+      }
+
+      console.log("review added successfully");
+      res.json({ message: "review added successfully" });
+    });
+
+    app.get("/review/:id", async (req, res) => {
+      const productId = req.params.id;
+
+      const result = await bookCollection.findOne(
+        { _id: new ObjectId(productId) },
+        { projection: { _id: 0, reviews: 1 } }
+      );
+
+      if (result) {
+        res.json(result);
+      } else {
+        res.status(404).json({ error: "REview not found" });
+      }
     });
   } finally {
     // Ensures that the client will close when you finish/error
